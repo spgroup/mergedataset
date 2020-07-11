013a3e7751d09b909eff9518d629ae9205bfbe86
Galileu Santos <galilasmb@gmail.com>
2020-07-11 19:48:26 -0300

    Renaming the path

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Base/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Base/saida.jar
-cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Left/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Left/saida.jar
-cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Merge/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Merge/saida.jar
-cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Right/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Right/saida.jar
-cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Base/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Base/saida.jar
-cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Left/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Left/saida.jar
-cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Merge/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Merge/saida.jar
-cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Right/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Right/saida.jar
-cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Base/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Base/saida.jar
-cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Left/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Left/saida.jar
-cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Merge/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Merge/saida.jar
-cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Right/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Right/saida.jar
+cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Base/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Base/buildFinal.jar
+cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Left/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Left/buildFinal.jar
+cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Merge/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Merge/buildFinal.jar
+cat spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Right/build* > spring-boot/958a0a45f164601d01cb706c19f22ed3e25eff56/original/Right/buildFinal.jar
+cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Base/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Base/buildFinal.jar
+cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Left/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Left/buildFinal.jar
+cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Merge/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Merge/buildFinal.jar
+cat spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Right/build* > spring-boot/c93ea54ea3e08eaa2a17640d5d2b3e60264c1a9c/original/Right/buildFinal.jar
+cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Base/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Base/buildFinal.jar
+cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Left/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Left/buildFinal.jar
+cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Merge/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Merge/buildFinal.jar
+cat spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Right/build* > spring-boot/fdd3f12ee0f92ac18844c08bf71df39feebb6673/original/Right/buildFinal.jar
 
 
